75e31dbacf9a5c66c5c99a72af2fb7f75af8fa7f
favour-olumese <olusafa1998@gmail.com>
2022-03-12 12:39:43 -0800

    A C program that checks if the last digit of a randomly generated number is either 0, less than 6, or greater than 5

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,11 +16,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	last_dt = n % 10;
-	if (n > 5)
+	if (last_dt > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, last_dt);
 	}
-	else if (n == 0)
+	else if (last_dt == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, last_dt);
 	}
