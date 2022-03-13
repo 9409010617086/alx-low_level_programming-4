4aad20ad54e707241a968c8bf5e2658e60f84b5a
favour-olumese <olusafa1998@gmail.com>
2022-03-13 08:46:10 -0700

    C program that prints all possible combinations of single-digit numbers separated by commas

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char num;
+	int i;
+
+	num = '0';
+
+	for (i = 0; i < 10; ++i)
+	{
+		if(i < 9)
+		{
+			putchar(num + i);
+			putchar(',');
+			putchar(' ');
+		}
+		else
+		{
+			putchar(num + i)
+		}
+	}
+
+	return (0);
+}
