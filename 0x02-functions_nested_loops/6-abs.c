5f26245d66f2ea9826960154d7d1f1895ab1489d
favour-olumese <olusafa1998@gmail.com>
2022-03-16 04:06:51 -0700

    function that prints the sign of a number.

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _abs(int) - function that computes the absolute value of an integer.
+ * _abs - function that computes the absolute value of an integer.
  * @n: An integer parameter.
  * Return: An int.
  */
