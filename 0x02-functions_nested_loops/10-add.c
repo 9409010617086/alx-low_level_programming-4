e29de15d0692c52f0f948e2c10f22b6a53668115
favour-olumese <olusafa1998@gmail.com>
2022-03-16 20:35:25 -0700

    function that adds two integers and returns the result.

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints the minutes of the day.
+ * @num1: An interger parameter.
+ * @num2: An interger parameter.
+ * Return: An int.
+ */
+int add(int num1, int num2)
+{
+	int add;
+
+	add = num1 + num2;
+
+	return (add);
+}
