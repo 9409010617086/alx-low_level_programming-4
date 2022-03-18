5cd75def31470ebbccd45be68a858aaa0b525c4f
favour-olumese <olusafa1998@gmail.com>
2022-03-18 06:41:59 -0700

    function that prints the decimal numbers, except 2 and 4 followed by a new line.

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function that prints the numbers, except 2 and 4 followed by a new line.
+ * 
+ * Return: Void.
+ */
+void print_most_numbers(void)
+{
+	int i;
+	char c;
+
+	c = '0';
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i == 2 || i == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(c + i);
+		}
+	}
+	_putchar('\n');
+}
