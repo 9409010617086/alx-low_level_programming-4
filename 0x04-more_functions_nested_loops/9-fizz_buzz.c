a885ffd60e4a45d703060df8bb9d07a0001dfcb5
favour-olumese <olusafa1998@gmail.com>
2022-03-19 13:31:19 -0700

    1 to 100, 3x replaced with Fizz, 5x replaced with Buzz, 3x and 5x replaced with FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,27 +8,28 @@ int main(void)
 {
 	int num;
 
-	for (num = 1; num < 101; num++)
+	for (num = 1; num <= 100; num++)
 	{
-		if (num == 100)
+		if (num % 3 == 0 && num % 5 == 0)
 		{
-			printf("Buzz");
-		}
-		else if (num % 3 == 0 && num % 5 == 0)
-		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if (num % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if (num % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else 
 		{
-			printf("%d ", num);
+			printf("%d", num);
+		}
+
+		if (num != 100)
+		{
+			printf(" ");
 		}
 	}
 	printf("\n");
