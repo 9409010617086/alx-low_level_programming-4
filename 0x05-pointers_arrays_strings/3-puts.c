fa0f19e7b56199a73f7d0125e8c84d394f0442bc
favour-olumese <olusafa1998@gmail.com>
2022-03-22 20:26:36 -0700

    function that prints a string, followed by a new line, to stdout.

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen - function that returns the length of a string.
+ *@str: Pointer parameter.
+ * Return: void.
+ */
+void _puts(char *str)
+{
+	int i;
+	int n;
+	
+	n = strlen(str);
+
+	for (i = 0; i <= n; i++)
+	{
+		_putchar(*(str + i));
+	}
+	_putchar('\n');
+}
