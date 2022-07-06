4deb8ae042adafc154e42e3f331d605b9b1f17f7
Favour Olumese <70489864+favour-olumese@users.noreply.github.com>
2022-07-06 01:46:44 +0100

    Updated betty documentation style.

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 /**
- * _alpha - Checks if a charcater is an alphabet.
+ * _isalpha - Checks if a charcater is an alphabet.
  * @c: An integer parameter.
  * Return: 0 if the character is not an alphabet, and 1 if it is.
  */
 
 int _isalpha(int c)
 {
-  if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
-  {
-    return (1);
-  }
-  else
-  {
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+	return (1);
+	}
+	else
+	{
 		return (0);
 	}
 }
