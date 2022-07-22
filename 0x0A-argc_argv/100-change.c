6993d6485e588e176b71c4f8fe068b299ce37ffe
Favour Olumese <70489864+favour-olumese@users.noreply.github.com>
2022-07-22 22:48:10 +0100

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -3,10 +3,8 @@
 
 /**
  * main - a program that prints the min no. of coins to make change for an amount of money.
- *
  * @argc: Interger parameter to count all number of arguments.
  * @argv: Argument vector.
- *
  * Return: 0 if successful, and 1 if unsuccessful.
  */
 
@@ -14,43 +12,37 @@ int main(int argc, char *argv[])
 {
 	int num = 0;
 	int cent = atoi(argv[1]);
-
+	
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
 	if (cent >= 25)
 	{
 		num += (cent / 25);
 		cent = cent % 25;
 	}
-
 	if (cent >= 10)
 	{
 		num += (cent / 10);
 		cent = cent % 10;
 	}
-
 	if (cent >= 5)
 	{
 		num += (cent / 5);
 		cent = cent % 5;
 	}
-
 	if (cent >= 2)
 	{
 		num += (cent / 2);
 		cent = cent % 2;
 	}
-
 	if (cent == 1)
 	{
 		num += (cent / 1);
 	}
 
 	printf("%d\n", num);
-
 	return (0);
 }
