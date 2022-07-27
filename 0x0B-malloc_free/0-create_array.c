122b2fbffd8f8b9d076362d47cafdffb0ffb7e19
Favour Olumese <70489864+favour-olumese@users.noreply.github.com>
2022-07-27 21:26:53 +0100

    Initialized all chars and made correction according to betty coding style.

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,8 +3,8 @@
 #include "main.h"
 
 /**
- * create_array - a function that creates an array of chars, and initializes it with a specific char.
- *
+ * create_array - Creates an array of chars, 
+ * and initializes it with a specific char.
  * @size: An unsigned integer parameter.
  * @c: A char parameter.
  *
@@ -14,15 +14,15 @@
 char *create_array(unsigned int size, char c)
 {
 	char *str;
+	unsigned int i;
 
 	str = (char *)malloc(sizeof(char) * size);
 
 	if (size == 0)
-	{
 		return (NULL);
-	}
 
-	*str = c;
+	for (i = 0; i < size; i++}
+		*str = c;
 
 	return (str);
 }
