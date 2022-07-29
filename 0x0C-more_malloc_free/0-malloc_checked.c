c4e2b31262969807ee82cd6c2ec5e9787882809b
Favour Olumese <70489864+favour-olumese@users.noreply.github.com>
2022-07-29 15:55:04 +0100

    Update 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -10,12 +10,13 @@
  * Return: Status value of 98 if it fails,
  * else a pointer to the allocated memory.
  */
+ 
 void *malloc_checked(unsigned int b)
 {
 	void *str = malloc(b);
 
 	if (str == NULL)
-		exit (98);
+		exit(98);
 
 	return (str);
 }
