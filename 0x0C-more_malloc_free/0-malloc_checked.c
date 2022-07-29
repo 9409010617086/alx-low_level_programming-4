ba499abaa8b358493bc3c2d1d308d8c46ca4adea
Favour Olumese <70489864+favour-olumese@users.noreply.github.com>
2022-07-29 15:58:56 +0100

    Removed <stdio.h>

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,12 +1,9 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
  * malloc_checked - a function that allocates memory using malloc.
- *
  * @b: An unsigned int parameter.
- *
  * Return: Status value of 98 if it fails,
  * else a pointer to the allocated memory.
  */
