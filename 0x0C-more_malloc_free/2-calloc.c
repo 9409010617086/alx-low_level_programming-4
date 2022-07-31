05d5a93e441f51fbeeb1de7000ca7fa3fd951ba3
Favour Olumese <70489864+favour-olumese@users.noreply.github.com>
2022-07-31 22:57:59 +0100

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -25,7 +25,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (call_oc == NULL)
 		return (NULL);
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < (size * nmemb); i++)
 		call_oc[i] = '\0';
 
 	return ((void *)call_oc);
