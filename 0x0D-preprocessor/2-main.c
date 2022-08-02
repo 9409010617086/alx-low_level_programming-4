cdd9302ddb99545db7a18d92d56aeba00125fc07
Favour Olumese <70489864+favour-olumese@users.noreply.github.com>
2022-08-02 11:30:30 +0100

    Added return (0)

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -3,9 +3,11 @@
 /**
  * main - prints the file name it was compiled from, followed by a new line.
  *
- * Return: Void.
+ * Return: Always 0.
  */
 int main(void)
 {
         printf("%s\n", __FILE__);
+
+        return (0);
 }
