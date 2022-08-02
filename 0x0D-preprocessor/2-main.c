18df215ffc46a99f3f9980b98a3b7b3eca5713bc
Favour Olumese <70489864+favour-olumese@users.noreply.github.com>
2022-08-02 11:35:47 +0100

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints the file name it was compiled from, followed by a new line.
+ * main - prints the file name it was compiled from.
  *
  * Return: Always 0.
  */
