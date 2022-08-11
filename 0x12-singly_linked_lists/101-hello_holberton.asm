d444de40c3e7e1573ec0a8813831d1cc6f5ba451
favour-olumese <olumesefavor@gmail.com>
2022-08-11 15:26:58 -0700

    Added description

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,6 +1,10 @@
+/**
+ * main - Hello, Holberton
+ * Return: Always 0.
+ */
 int main(void)
 {
-  register char* arg2 asm("rsi") = "Hello, Holberton\n";
-  asm("mov $1, %rax; mov $1, %rdi; mov $14, %rdx; syscall;");
-  return 0;
+	register char* arg2 asm("rsi") = "Hello, Holberton\n";
+	asm("mov $1, %rax; mov $1, %rdi; mov $14, %rdx; syscall;");
+	return (0);
 }
