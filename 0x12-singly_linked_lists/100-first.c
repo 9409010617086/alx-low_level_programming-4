1a69cb0e1c8f3b9efcb20f4538cf0598fd758b2d
favour-olumese <olumesefavor@gmail.com>
2022-08-11 14:30:41 -0700

    Function that prints before main is executed.

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* The constructor attribute causes before_main() to excute before main(). */
+void before_main (void) __attribute__ ((constructor));
+
+/**
+ * before_main - Function that prints before main is executed.
+ */
+void before_main(void)
+{
+    printf("You're beat! and yet, you must allow,\
+    \nI bore my house upon my back!\n");
+}
