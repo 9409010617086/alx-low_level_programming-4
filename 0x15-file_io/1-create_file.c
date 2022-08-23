657b15c0003a7062ed59c5aead6269fae23479ce
favour-olumese <olumesefavor@gmail.com>
2022-08-23 05:22:26 -0700

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,12 +20,12 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	if (fd == -1)
-		return (-1);
-
 	/* Create the file if it does not exist with rw-------, else
 	truncate ita with it current permissions. */
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
 
 	/* Add text_content to filename if it is not empty */
 	if (text_content != NULL)
